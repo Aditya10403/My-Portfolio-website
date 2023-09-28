6197c4a162e79b23b4692c452918d6b6418509cd
Aditya10403 <adityashukla@gmail.com>
2023-09-28 10:15:37 +0530

    updated

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzWk_uHPhzQ0naIvNsPPtVjVtij9KrZxYaHicKKWh-70b7GpIhgvX0DX2lHyjR0PGLa/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message send successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 8000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
+
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname) {
